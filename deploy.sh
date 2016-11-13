d46fdf85e7cbda883fb919c3fd8231883f967f7d
Ivan Varga <ivan.varga.zg@gmail.com>
2016-11-13 14:47:06 +0100

    aws region

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ EB_BUCKET=web-boilerpplate
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws configure set default.region us-west-2
 aws elasticbeanstalk create-application-version --application-name web-boilerplate \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
