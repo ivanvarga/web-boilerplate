13a9ab073ab9709e5f13358704913d9b76916858
Ivan Varga <ivan.varga.zg@gmail.com>
2016-11-13 15:16:26 +0100

    aws eb

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ docker push ivanvarga/web-boilerplate:$SHA1
 # Create new Elastic Beanstalk version
 EB_BUCKET=web-boilerpplate
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name web-boilerplate \
