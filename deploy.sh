aef7ceb2be79c3374ab3d4a382abda4d5a0407aa
Ivan Varga <ivan.varga.zg@gmail.com>
2016-11-13 01:33:42 +0100

    test

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 SHA1=$1
 
 # Deploy image to Docker Hub
-docker push circleci/hello:$SHA1
+docker push ivanvarga/web-boilerpplate:$SHA1
 
 # Create new Elastic Beanstalk version
-EB_BUCKET=hello-bucket
+EB_BUCKET=web-boilerpplate
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
