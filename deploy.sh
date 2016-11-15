d38a0e88f7975bc232874c3d3841180b57f8df31
Ivan Varga <ivan.varga.zg@gmail.com>
2016-11-15 12:24:53 +0100

    latest docker image

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@
 SHA1=$1
 
 # Deploy image to Docker Hub
-docker push ivanvarga/web-boilerplate:$SHA1
+docker push ivanvarga/web-boilerplate:latest
 
 # Create new Elastic Beanstalk version
 EB_BUCKET=web-boilerplate
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 
-sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+sed "s/<TAG>/latest/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name web-boilerplate \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
