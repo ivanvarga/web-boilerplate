e28ed996caa2ef8e245cc8a0fd77750192616923
Dino <dino.koprivnjak@axilis.com>
2016-11-16 14:46:06 +0100

    add eslint script to package.json

--- src/server/app.js
+++ src/server/app.js
@@ -11,10 +11,10 @@ const port = 5000;
 app.use('/', express.static(path.resolve(__dirname, '../client/public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(path.resolve(__dirname, '../client/index.html')));
+  res.sendFile(path.join(path.resolve(__dirname, '../client/index.html')));
 });
 /* eslint-disable no-console */
 app.listen(port, () => {
-    console.log(`New server started on port:${port}`);
+  console.log(`New server started on port:${port}`);
 });
 /* eslint-enable no-console */
